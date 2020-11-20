431d4077ddefc5a5d0bb8cd4c0807dc45f7b442e
IlyaMalyukov <ilya.maliukov@yandex.ru>
2020-11-20 22:59:12 +0400

    Создание маршрутов

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
   end
+
+  def client
+  end
+
+  def workers
+  end
+
+  def ads
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root 'static_pages#home'
+  get 'help' => 'static_pages#help'
+  #get 'clients' => 'static_pages/clients'
+  get 'clients' => 'static_pages#clients'
+  get 'workers' => 'static_pages#workers'
+  get 'ads' => 'static_pages#ads'
+  get 'signup' => 'users#new'
 end
