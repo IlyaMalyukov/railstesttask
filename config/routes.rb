6d1ab0ac055a9c4613cbd0c71ca0ffbf740cb830
IlyaMalyukov <ilya.maliukov@yandex.ru>
2020-11-23 22:59:49 +0400

    Вход/Выход

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'users/new'
   root 'static_pages#home'
   get 'help' => 'static_pages#help'
-  #get 'clients' => 'static_pages/clients'
   get 'clients' => 'static_pages#clients'
   get 'workers' => 'static_pages#workers'
   get 'ads' => 'static_pages#ads'
   get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   resources :users
 end
